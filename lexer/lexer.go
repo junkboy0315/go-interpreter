f1ebfa1a1aed89e753b994f87ece74ad4277b88f
Shota Tamura <tamura@yuuniworks.com>
2019-03-28 16:38:06 +0900

    implement basic lexer

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,8 @@
 package lexer
 
-import "github.com/junkboy0315/go-interpreter/token"
+import (
+	"github.com/junkboy0315/go-interpreter/token"
+)
 
 // Lexer は文字列をトークンに変換する
 type Lexer struct {
@@ -20,6 +22,8 @@ func CreateNewLexer(input string) *Lexer {
 // NextToken は現在位置のトークンを返す
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	l.skipWhitespace()
 
 	switch l.ch {
 	case '=':
@@ -41,6 +45,20 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupTokenType(tok.Literal)
+			// 既にreadChar()は完了しているため、早期にリターンする
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readDigit()
+			tok.Type = token.INT
+			// 既にreadChar()は完了しているため、早期にリターンする
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.readChar()
 	return tok
@@ -55,6 +73,39 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition++
+}
+
+// 現在位置から、次の非英字の直前までを、文字列で返す
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// 現在位置から、次の非数字の直前までを、文字列で返す
+func (l *Lexer) readDigit() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// 空白文字等を読み飛ばす
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
 
 func newToken(tokenType token.Type, ch byte) token.Token {
--- token/token.go
+++ token/token.go
@@ -3,19 +3,16 @@ package token
 // Type はトークンの種類を表す文字列
 type Type string
 
-// Token は`トークンの種類`と`実際のコードの文字列`を持つ
+// Token はトークンの種類と実際の値を持つ
 type Token struct {
 	Type    Type
 	Literal string
 }
 
-// Typeの値
+// トークンの種類を表す定数
 const (
 	ILLEGAL = "ILLEGAL" // 解析不能な字句
 	EOF     = "EOF"
-
-	// 識別子
-	IDENT = "IDENT" // x, y など
 
 	// リテラル
 	INT = "INT" // 整数 1, 2, 3 など
@@ -32,7 +29,26 @@ const (
 	LBRACE    = "{"
 	RBRACE    = "}"
 
-	// キーワード
+	// ユーザ定義の識別子
+	IDENT = "IDENT" // x, y など
+
+	// 定義済みの識別子（キーワード）
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+// 定義済みの識別子とトークンの種類の関連付け
+var tokenTypeByKeyword = map[string]Type{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// LookupTokenType は、与えられた文字列を基に、トークンの種類を判断して返す
+func LookupTokenType(ident string) Type {
+	// 与えられた文字列が、定義済みキーワードだった場合(`fn`, `let`など)
+	if tokenType, ok := tokenTypeByKeyword[ident]; ok {
+		return tokenType
+	}
+	// 与えられた文字列が、ユーザ定義の識別子だった場合
+	return IDENT
+}
